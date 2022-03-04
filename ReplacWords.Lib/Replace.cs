0b1112d6af946cc23a8c2a884cefc2bb65410078
Pavel Popov <legeon48@mail.ru>
2022-03-04 18:20:28 +0300

    создан класс для замены слова

--- ReplacWords.Lib/Replace.cs
+++ ReplacWords.Lib/Replace.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ReplacWords.Lib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,40 @@ using System.Threading.Tasks;
 
 namespace ReplacWords.Lib
 {
-    internal class Replace
+    public class Replace
     {
+        //с-во, присваивается слово которое изменяем 
+        public string Word { get; set; }
+
+        //конструктор по умолчанию
+        public Replace() { }
+
+        //конструктор с параметрами
+        public Replace(string word) => Word = word;
+
+        //метод изменяет слово на ****
+        public void ReplacementWord()
+        {
+            if (Word is not null && Word != "")
+            {
+                int size = Word.Length;
+                Word = "";
+
+                for (int i = 0; i < size; i++)
+                {
+                    Word += "*";
+                }
+            }
+
+        }
+
+        //асинхронный метод изменения слова
+        public async Task ReplacementWordAsync()
+        {
+            await Task.Run(() => ReplacementWord());
+        }
+
+        //так для разнообразия
+        public override string ToString() => $"{Word}";
     }
 }
