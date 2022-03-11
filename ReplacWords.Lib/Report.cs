2fa35e54d597d9db9998141f4369b917823dd2f7
Kirill Lagutin <lagutin.k@mail.ru>
2022-03-11 20:57:32 +0300

    Update

--- ReplacWords.Lib/Report.cs
+++ ReplacWords.Lib/Report.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
 
 namespace ReplacWords.Lib
 {
@@ -9,10 +10,8 @@ namespace ReplacWords.Lib
         //Путь к папке с файлами найденных слов
         static string pathDir = "";
         
-        Report(string _path)
-        {
-            pathDir = _path;
-        }
+        //Путь к файлу для записи информации
+        string pathForWrite = @"D:\!Desktop\Step\C#\Projects\ReplacWords\file_report.txt";
         
         //Массив файлов найденных слов в папке
         string[] bannedWordFile = Directory.GetFiles(pathDir, "*.txt");
@@ -20,9 +19,6 @@ namespace ReplacWords.Lib
         //Запись информации в файл
         public async Task WriteInfoToFile(string[] bannedWordFile)
         {
-            //Путь к файлу для записи информации
-            string pathForWrite = @"D:\!Desktop\Step\C#\Projects\ReplacWords\file_report.txt";
-
             using (StreamWriter sw = new StreamWriter(pathForWrite, true))
             {
                 int CountAll = 0;
@@ -31,43 +27,9 @@ namespace ReplacWords.Lib
                 {
                     await sw.WriteLineAsync($"Путь файла №{i + 1}: {Path.GetFullPath(bannedWordFile[i])}");
                     await sw.WriteLineAsync($"Размер файла №{i + 1}: {bannedWordFile[i].Length}");
-                    await sw.WriteLineAsync("Количество замен в файле №" + i + 1 + ":" + CountForbiddenWords(bannedWordFile[i]));
-                    CountAll += CountForbiddenWords(bannedWordFile[i]);
                 }
 
-                await sw.WriteLineAsync($"Общее колличество найденных файлов: {CountAll}");
-            }
-        }
-        
-        // Подсчёт найденных запрещённых слов в файле
-        public int CountForbiddenWords(string path)
-        {
-            int wordsCounter = 0;
-
-            StreamReader sr = new StreamReader(path);
-            
-            string word = "";
-            while (!sr.EndOfStream)
-            {
-                word += sr.ReadLine();
-            }
-
-            string[] wordArray = word.Split(new char[] {' ',','});
-            foreach (string str in wordArray)
-            {
-                wordsCounter++;
-            }
-
-            return wordsCounter;
-        }
-        
-        //Вывод информации на экран
-        public async Task Show(string filePath)
-        {
-            using(StreamReader sr = new StreamReader(filePath))
-            {
-                string textInfo = await sr.ReadToEndAsync();
-                Console.WriteLine(textInfo);
+                await sw.WriteLineAsync($"Общее колличество найденных файлов: {Replace.NumberOfSubstitutions}");
             }
         }
     }
