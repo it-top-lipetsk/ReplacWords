ad23046b201be95392d35688e8e6b45e31f7f09e
Pavel Popov <legeon48@mail.ru>
2022-03-11 20:47:55 +0300

    Тест

---
+++ TestProjectReplace/UnitTest1.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReplacWords.Lib;
+
+namespace TestProjectReplace
+{
+    [TestClass]
+    public class UnitTestReplace
+    {
+        [TestMethod]
+        public void TestReplace()
+        {
+            var words = new string[] { "df", "123", "qwerty" };
+            var str = "Qwerty DF df 123 df123df";
+            var replace = new Replace(words, str);
+
+            var expected = "****** ** ** *** df123df";
+            var actual = replace.ReplacementWord();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
