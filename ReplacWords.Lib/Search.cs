64760fc6f03b6e54fac6b87946984cfbafe9d729
AlekseiTeterin <alexey_teterin@mail.ru>
2022-03-11 18:04:48 +0300

    Учтены комментарии к тексту программы. Код поиска искомых слов выделен в отдельную функцию.
    Пока осталось разобраться с тем, чтобы просматривались не только файлы ТХТ

--- ReplacWords.Lib/Search.cs
+++ ReplacWords.Lib/Search.cs
@@ -9,42 +9,56 @@ namespace ReplacWords.Lib
 {
     class Search
     {
-        const int size = 10;
-        string[] forbiddenWords = new string[size];
-        static string docsPath =
-             Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-        static string controlFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/ForbiddenFiles";
-
-        // конструктор по умолчанию
-        public Search() { }
-
-        public Search(string anotherPath) { docsPath = anotherPath; }
+        public const int size = 10;
+        public string[] forbiddenWords;
+        public string docsPath;
+        public string controlFolder;
+        //static string docsPath =
+        //     Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        //internal static string controlFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/ForbiddenFiles";
+                
+        public Search(string NewDocsPath) 
+        {            
+            forbiddenWords = new string[size];
+            // плюсом не забыть добавить запрещенные слова в массив для поиска по тексту 
+            docsPath = NewDocsPath;
+            controlFolder = docsPath + "/ForbiddenFiles";
+        }
 
         public void StartSearch()
         {
-
             var Files = Directory.EnumerateFiles(docsPath, "*.txt", SearchOption.AllDirectories);
-
 
             foreach (string currentFile in Files)
             {
-
-                string line;
-                StreamReader f = new StreamReader(currentFile);
-                while ((line = f.ReadLine()) != null)
+                if(SearchingWordsInFile(currentFile))
                 {
-                    for(int i = 0; i < size; i++)
-                    {
-                        if(line.Contains(forbiddenWords[i]))
-                        {
-                            string fileName = currentFile.Substring(docsPath.Length + 1);
-                            File.Copy(currentFile, Path.Combine(controlFolder, fileName));
-                        }
-                    }
+                    string fileName = currentFile.Substring(docsPath.Length + 1);
+                    File.Copy(currentFile, Path.Combine(controlFolder, fileName));
                 }
-                f.Close();
-               
-            }          
+            }           
         }
+        public bool SearchingWordsInFile(string filePath)
+        {
+            using (FileStream fstream = File.OpenRead(filePath))
+            {
+                // выделяем массив для считывания данных из файла
+                byte[] buffer = new byte[fstream.Length];
+                // считываем данные
+                fstream.Read(buffer, 0, buffer.Length);
+                // декодируем байты в строку
+                string textFromFile = Encoding.Default.GetString(buffer);
+                //Console.WriteLine($"Текст из файла: {textFromFile}");
+
+                for (int i = 0; i < size; i++)
+                {
+                    if (textFromFile.Contains(forbiddenWords[i]))
+                    {
+                        return true;
+                    }     
+                }
+                return false;
+            }
+        }   
     }
 }
