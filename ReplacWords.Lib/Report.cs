615ccb393a1ad054dbfa02cf6554a24e0a327a5b
Kirill Lagutin <lagutin.k@mail.ru>
2022-03-12 00:51:07 +0300

    Просьба к Алексею, добавить метод в его классе.

--- ReplacWords.Lib/Report.cs
+++ ReplacWords.Lib/Report.cs
@@ -1,25 +1,20 @@
 ﻿using System.IO;
 using System.Threading.Tasks;
 
-
 namespace ReplacWords.Lib
 {
     public class Report
     {
-        //Массив файлов с запрещёнными словами
-        string[] bannedWordFile = Directory.GetFiles(Search.controlFolder, "*.txt");
-        
-        //Запись информации в файл
-        public async Task WriteInfoToFile(string[] bannedWordFile)
+        public async Task WriteInfoToFile()
         {
             using (StreamWriter sw = new StreamWriter(Path.GetFullPath("file_info.txt"), true))
             {
-                await sw.WriteLineAsync($"Найдено {bannedWordFile.Length} файла(ов) с запрещёнными словами!");
+                await sw.WriteLineAsync($"Найдено {Search.ArrBannedWordFile.Length} файла(ов) с запрещёнными словами!");
                 
-                for (int i = 0; i < bannedWordFile.Length; i++)
+                for (int i = 0; i < Search.ArrBannedWordFile.Length; i++)
                 {
-                    await sw.WriteLineAsync($"Размер файла №{i + 1}: {new FileInfo(bannedWordFile[i]).Length}");
-                    await sw.WriteLineAsync($"Путь файла №{i + 1}: {Path.GetFullPath(bannedWordFile[i])}");
+                    await sw.WriteLineAsync($"Размер файла №{i + 1}: {new FileInfo(Search.ArrBannedWordFile[i]).Length}");
+                    await sw.WriteLineAsync($"Путь файла №{i + 1}: {Path.GetFullPath(Search.ArrBannedWordFile[i])}");
                 }
 
                 await sw.WriteLineAsync($"Общее колличество найденных файлов: {Replace.NumberOfSubstitutions}");
