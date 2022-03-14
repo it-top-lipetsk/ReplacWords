f8627cc0fb0417ebe531465032c075f0ea5df81c
Aleksei Teterin <alexey_teterin@mail.ru>
2022-03-14 14:48:31 +0300

    - файл считывается сразу в строку при помощи метода ReadToEnd(); - для Кирилла полные имена файлов(пути) добавляются в массив ArrBannedWordFile[]; - добавлена возможность проверки файлов не только с расширением ТХТ, а и с другими расширениями; - добавлен асинхронный метод StartSearchAsync();

--- ReplacWords.Lib/Search.cs
+++ ReplacWords.Lib/Search.cs
@@ -10,46 +10,49 @@ namespace ReplacWords.Lib
     class Search
     {
         public const int size = 10;
-        public string[] forbiddenWords;
+        public string[] forbiddenWords; //массив запрещенных слов
+        public static string[] filesExtensions; //список расширений файлов, которые будут проверяться
         public string docsPath;
         public string controlFolder;
-        //static string docsPath =
-        //     Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-        //internal static string controlFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/ForbiddenFiles";
-                
+        public static string[] ArrBannedWordFile; //for @KirillLagutin
+        
         public Search(string NewDocsPath) 
         {            
             forbiddenWords = new string[size];
-            // плюсом не забыть добавить запрещенные слова в массив для поиска по тексту 
+            // плюсом не забыть добавить запрещенные слова в массив для поиска по тексту или подставить из общего
+            filesExtensions = new string[] { "*.txt", "*.doc", "*.docx", "*.rtf", "*.djvu", "*.pdf", "*.odt", };
             docsPath = NewDocsPath;
             controlFolder = docsPath + "/ForbiddenFiles";
         }
 
         public void StartSearch()
         {
-            var Files = Directory.EnumerateFiles(docsPath, "*.txt", SearchOption.AllDirectories);
+            for(int i = 0; i < filesExtensions.Length; i++)
+            {
+                SearchOneExt(filesExtensions[i]);
+            }
+        }
 
+        public void SearchOneExt(string fileExt)
+        {
+            var Files = Directory.EnumerateFiles(docsPath, fileExt , SearchOption.AllDirectories);
+            int SizeArrBannedWordFile = 0;
             foreach (string currentFile in Files)
             {
                 if(SearchingWordsInFile(currentFile))
                 {
+                    ArrBannedWordFile[SizeArrBannedWordFile] = currentFile; //полное имя файла(путь) с запрещёнными словами копируется в стринговый массив 
+                    SizeArrBannedWordFile++;
                     string fileName = currentFile.Substring(docsPath.Length + 1);
                     File.Copy(currentFile, Path.Combine(controlFolder, fileName));
                 }
             }           
         }
         public bool SearchingWordsInFile(string filePath)
-        {
-            using (FileStream fstream = File.OpenRead(filePath))
+        {                      
+            using (StreamReader sr = new StreamReader(filePath))
             {
-                // выделяем массив для считывания данных из файла
-                byte[] buffer = new byte[fstream.Length];
-                // считываем данные
-                fstream.Read(buffer, 0, buffer.Length);
-                // декодируем байты в строку
-                string textFromFile = Encoding.Default.GetString(buffer);
-                //Console.WriteLine($"Текст из файла: {textFromFile}");
-
+                string textFromFile = sr.ReadToEnd();
                 for (int i = 0; i < size; i++)
                 {
                     if (textFromFile.Contains(forbiddenWords[i]))
@@ -59,6 +62,12 @@ namespace ReplacWords.Lib
                 }
                 return false;
             }
-        }   
+        }
+
+        //асинхронная версия метода поиска файлов с запрещёнными словами
+        public async Task StartSearchAsync()
+        {
+            await Task.Run(() => StartSearch());
+        }
     }
 }
