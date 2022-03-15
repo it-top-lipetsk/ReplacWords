1947571e73b2014962992f7482bff50119e45877
Pavel Popov <legeon48@mail.ru>
2022-03-15 20:17:50 +0300

    исправлен тест

--- TestProjectReplace/UnitTest1.cs
+++ TestProjectReplace/UnitTest1.cs
@@ -15,11 +15,24 @@ namespace TestProjectReplace
 
             var expected = "****** ** ** ***, zxdd *******";
             var actual = replace.ReplacementWord();
-            var countExpected = 7;
-            var actualExpected = replace.NumberOfSubstitutions;
+            
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestReplaceNumberOfSubstitutions()
+        {
+            var words = new string[] { "df", "123", "qwerty" };
+            var str = "Qwerty DF df 123, zxdd df123df";
+            var replace = new Replace(words, str);
+
+            replace.ReplacementWord();
+
+            var expected = 7;
+            var actual = replace.NumberOfSubstitutions;
 
             Assert.AreEqual(expected, actual);
-            Assert.AreEqual(countExpected, actualExpected);
+            
         }
     }
 }
