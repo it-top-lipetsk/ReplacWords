5e22f2dff1d136dbb85ae0c515402d3743c778fb
Starinin Andrey <starininandrey@gmail.com>
2022-03-18 19:10:05 +0300

    Добавление GUI

--- ReplacWords.Lib/Report.cs
+++ ReplacWords.Lib/Report.cs
@@ -7,18 +7,17 @@ namespace ReplacWords.Lib
     {
         public async Task WriteInfoToFile()
         {
-            using (StreamWriter sw = new StreamWriter(Path.GetFullPath("file_info.txt"), true))
-            {
-                await sw.WriteLineAsync($"Найдено {Search.ArrBannedWordFile.Length} файла(ов) с запрещёнными словами!");
+            await using StreamWriter sw = new StreamWriter(Path.GetFullPath("file_info.txt"), true);
+            await sw.WriteLineAsync($"Найдено {Search.ArrBannedWordFile.Count} файла(ов) с запрещёнными словами!");
                 
-                for (int i = 0; i < Search.ArrBannedWordFile.Length; i++)
-                {
-                    await sw.WriteLineAsync($"Размер файла №{i + 1}: {new FileInfo(Search.ArrBannedWordFile[i]).Length}");
-                    await sw.WriteLineAsync($"Путь файла №{i + 1}: {Path.GetFullPath(Search.ArrBannedWordFile[i])}");
-                }
-
-                await sw.WriteLineAsync($"Общее колличество найденных файлов: {Replace.NumberOfSubstitutions}");
+            for (int i = 0; i < Search.ArrBannedWordFile.Count; i++)
+            {
+                await sw.WriteLineAsync($"Размер файла №{i + 1}: {new FileInfo(Search.ArrBannedWordFile[i]).Length}");
+                await sw.WriteLineAsync($"Путь файла №{i + 1}: {Path.GetFullPath(Search.ArrBannedWordFile[i])}");
             }
+
+            //TODO
+            //await sw.WriteLineAsync($"Общее колличество найденных файлов: {Replace.NumberOfSubstitutions}");
         }
     }
 }
